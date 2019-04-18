0bb1f2b9c90894cf3b6254c09372ae9570953780
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-04-18 16:15:46 +0200

    add post process files for the test cases1, 2, 3

---
+++ examples/case3/pol.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rc('font', size=15)
+
+file_name = "powerlaw_L_dependency.txt"
+data = np.loadtxt(file_name, dtype=np.int, delimiter=",")
+
+num_steps = 5
+L = 0.1*np.arange(101)
+
+for step in np.arange(num_steps):
+    plt.plot(L, data[:, step])
+    plt.ylabel("$\sharp$")
+    plt.xlabel("$L$")
+    plt.grid(True)
+    file_name = "cross_" + str(step) + ".pdf"
+    plt.savefig(file_name, bbox_inches='tight')
+    plt.gcf().clear()
